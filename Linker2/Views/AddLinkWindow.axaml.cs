4027b7c7df1acedf22b888417c0ba064331289ce
Pontus Markström <pontus.markstrom@gmail.com>
2024-02-01 19:17:51 +0100

    Fix cancel button for edit/add link window

--- Linker2/Views/AddLinkWindow.axaml.cs
+++ Linker2/Views/AddLinkWindow.axaml.cs
@@ -18,5 +18,10 @@ namespace Linker2.Views
             this.RegisterForEvent<SessionStopped>((x) => Close());
             this.RegisterForEvent<CloseDialog>((x) => Close());
         }
+
+        private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
