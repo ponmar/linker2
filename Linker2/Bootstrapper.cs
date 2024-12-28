3558e588c2027e994345efd7f4e97e9eb77fafcf
Pontus Markström <pontus.markstrom@gmail.com>
2024-12-28 23:18:47 +0100

    Improved bootstrapping code (WindsorContainer moved to ServiceLocator)

--- Linker2/Bootstrapper.cs
+++ Linker2/Bootstrapper.cs
@@ -1,33 +1,32 @@
-﻿using Castle.MicroKernel.Registration;
-using Castle.Windsor;
-using Linker2.Model;
+﻿using Linker2.Model;
 using Linker2.ViewModels;
 using System.IO.Abstractions;
 
 namespace Linker2;
 
 public static class Bootstrapper
-{
-    public static WindsorContainer Container { get; } = new WindsorContainer();
-
+{ 
     public static void Bootstrap()
     {
-        Container.Register(Component.For<IFileSystem>().ImplementedBy<FileSystem>());
-        Container.Register(Component.For<IFileUtils>().ImplementedBy<FileUtils>());
-        Container.Register(Component.For<IDialogs>().ImplementedBy<Dialogs>());
-        Container.Register(Component.For(
+        ServiceLocator.RegisterSingleton<IFileSystem, FileSystem>();
+        ServiceLocator.RegisterSingleton<IFileUtils, FileUtils>();
+        ServiceLocator.RegisterSingleton<IDialogs, Dialogs>();
+
+        ServiceLocator.RegisterSingleton<Model.Model>([
             typeof(ILinkRepository),
             typeof(ISettingsProvider),
             typeof(ILinkModification),
             typeof(ISessionSaver),
             typeof(ISessionUtils),
-            typeof(IWebPageScraperProvider)).ImplementedBy<Model.Model>());
-        Container.Register(Component.For<IClipboardService>().ImplementedBy<ClipboardService>());
+            typeof(IWebPageScraperProvider)]);
 
-        Container.Register(Component.For<AddLinkViewModel>().ImplementedBy<AddLinkViewModel>().LifestyleTransient());
-        Container.Register(Component.For<CreateViewModel>().ImplementedBy<CreateViewModel>().LifestyleTransient());
-        Container.Register(Component.For<PasswordViewModel>().ImplementedBy<PasswordViewModel>().LifestyleTransient());
-        Container.Register(Component.For<MainViewModel>().ImplementedBy<MainViewModel>());
-        Container.Register(Component.For<LinksViewModel>().ImplementedBy<LinksViewModel>());
+        ServiceLocator.RegisterSingleton<IClipboardService, ClipboardService>();
+
+        ServiceLocator.RegisterTransient<AddLinkViewModel>();
+        ServiceLocator.RegisterTransient<CreateViewModel>();
+        ServiceLocator.RegisterTransient<PasswordViewModel>();
+
+        ServiceLocator.RegisterSingleton<MainViewModel>();
+        ServiceLocator.RegisterSingleton<LinksViewModel>();
     }
 }
--- Linker2/ServiceLocator.cs
+++ Linker2/ServiceLocator.cs
@@ -1,12 +1,56 @@
-﻿using Castle.MicroKernel;
+﻿using System;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
 
 namespace Linker2;
 
 public class ServiceLocator
 {
-    public static T Resolve<T>()
+    private static WindsorContainer Container { get; } = new();
+
+    public static void RegisterSingleton<T>() where T : class
     {
-        return Bootstrapper.Container.Resolve<T>();
+        RegisterSingleton<T, T>();
+    }
+
+    public static void RegisterSingleton<TService, TImplementedBy>() where TService : class where TImplementedBy : TService
+    {
+        Container.Register(Component.For<TService>().ImplementedBy<TImplementedBy>());
+    }
+
+    public static void RegisterSingleton<TImplementedBy>(params Type[] services) where TImplementedBy : class
+    {
+        foreach (var service in services)
+        {
+            var implementedByType = typeof(TImplementedBy);
+            if (!service.IsAssignableFrom(implementedByType))
+            {
+                throw new ArgumentException($"Registered type {implementedByType} does not implement {service}");
+            }
+        }
+
+        Container.Register(Component.For(services).ImplementedBy<TImplementedBy>());
+    }
+
+    public static void RegisterTransient<TServiceAndImplementation>() where TServiceAndImplementation : class
+    {
+        RegisterTransient<TServiceAndImplementation, TServiceAndImplementation>();
+    }
+
+    public static void RegisterTransient<TService, TImplementedBy>() where TService : class where TImplementedBy : TService
+    {
+        Container.Register(Component.For<TService>().ImplementedBy<TImplementedBy>().LifestyleTransient());
+    }
+
+    public static TService Resolve<TService>()
+    {
+        return Container.Resolve<TService>();
+    }
+
+    public static object Resolve(Type serviceType)
+    {
+        return Container.Resolve(serviceType);
     }
 
     public static T Resolve<T>(string key, object? value)
@@ -20,6 +64,6 @@ public class ServiceLocator
         {
             { key, value }
         };
-        return Bootstrapper.Container.Resolve<T>(a);
+        return Container.Resolve<T>(a);
     }
 }
