1d47355975fdd3c4cd9f8c113849333cbe10b804
Pontus Markström <pontus.markstrom@gmail.com>
2025-10-05 17:46:33 +0200

    Do not show enum value names for selected file availability filter

---
+++ Linker2/Converters/LinkFileAvailabilityToTextConverter.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Data.Converters;
+using Linker2.Configuration;
+using Linker2.Validators;
+using System;
+
+namespace Linker2.Converters;
+
+public class LinkFileAvailabilityToTextConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is LinkFileAvailability enumValue)
+        {
+            return enumValue switch 
+            {
+                LinkFileAvailability.Available => "Available",
+                LinkFileAvailability.NotAvailable => "Not available",
+                _ => string.Empty,
+            };
+        }
+        return string.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
